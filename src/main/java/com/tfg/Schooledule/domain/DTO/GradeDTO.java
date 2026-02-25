37501d0005e63b142574f85c5bdd8a5fc159872a
Rafael Medina <rafael.medina.ayuso@gmail.com>
2026-02-25 19:53:11 +0100

    feat(dto): Create DTOs for student profile and grades

---
+++ src/main/java/com/tfg/Schooledule/domain/DTO/GradeDTO.java
@@ -0,0 +1,21 @@
+package com.tfg.schooledule.domain.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GradeDTO {
+    private String itemNombre;
+    private BigDecimal valor;
+    private String comentario;
+    private LocalDate fecha;
+    private String tipoActividad;
+}
